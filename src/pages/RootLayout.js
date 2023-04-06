f403344cfedbc33055f6c346ce73d161050ef1ed
vergeldev <vergeldev@gmail.com>
2023-04-06 10:33:19 +0800

    Added some sidebar styling

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,24 +1,39 @@
 import { Outlet, Link } from 'react-router-dom'
 
 export default function RootLayout() {
+    const sidebarStyle =
+        'text-gray-900 text-center py-2 px-4 m-1 rounded hover:text-blue-900 hover:bg-blue-50 hover:border-l-4 hover:border-blue-900'
+
     return (
-        <>
-            <header>
-                <nav>
-                    <Link to="dashboard">Dashboard</Link>
-                    <Link to="orders">Orders</Link>
-                    <Link to="inventory">Inventory</Link>
-                    <Link to="logistics">Logistics</Link>
-                    <Link to="payments">Payments</Link>
-                    <Link to="analytics">Analytics</Link>
+        <div className="container flex">
+            <header className="w-1/5 h-screen">
+                <nav className="mt-8 flex flex-col items-center items-stretch">
+                    <Link to="dashboard" className={sidebarStyle}>
+                        Dashboard
+                    </Link>
+                    <Link to="orders" className={sidebarStyle}>
+                        Orders
+                    </Link>
+                    <Link to="inventory" className={sidebarStyle}>
+                        Inventory
+                    </Link>
+                    <Link to="logistics" className={sidebarStyle}>
+                        Logistics
+                    </Link>
+                    <Link to="payments" className={sidebarStyle}>
+                        Payments
+                    </Link>
+                    <Link to="analytics" className={sidebarStyle}>
+                        Analytics
+                    </Link>
                 </nav>
             </header>
 
-            <main>
+            <main className="w-4/5 h-screen bg-gray-200">
                 <p>test</p>
                 <Outlet />
             </main>
-        </>
+        </div>
     )
 }
 
